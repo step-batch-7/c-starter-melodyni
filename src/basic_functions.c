f7185da3e2da53a6713ae57cdcfa33999a393641
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-03-30 13:10:58 +0530

    added square function

--- src/basic_functions.c
+++ src/basic_functions.c
@@ -2,6 +2,7 @@
 
 char isEven(int num);
 char isOdd(int num);
+long int square(int num);
 
 char isEven(int num){
   return num % 2 == 0;
@@ -11,14 +12,22 @@ char isOdd(int num){
   return !isEven(num);
 };
 
+long int square(int num){
+  return num * num;
+};
+
 
 int main(void){
   int num;
   printf("Enter a number to check whether it is even or not: ");
   scanf("%d", &num);
-  printf("%s",isEven(num)? "even":"not even");
+  printf("%d is %s", num, isEven(num)? "even":"not even");
 
   printf("\n\nEnter a number to check whether it is odd or not: ");
   scanf("%d", &num);
-  printf("%s",isOdd(num)? "odd":"not odd");
+  printf("%d is %s", num, isOdd(num)? "odd":"not odd");
+
+  printf("\n\nEnter a number to get square of it: ");
+  scanf("%d", &num);
+  printf("Square of %d is %ld", num, square(num));
 };
