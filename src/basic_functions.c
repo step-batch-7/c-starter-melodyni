24ecce1441930348952a3d8c84b61a16c1836360
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-03-30 13:52:32 +0530

    added simpleInterest function

--- src/basic_functions.c
+++ src/basic_functions.c
@@ -4,6 +4,7 @@ char isEven(int num);
 char isOdd(int num);
 long int square(int num);
 long int cube(int num);
+double simpleInterest(double,double,double);
 
 char isEven(int num){
   return num % 2 == 0;
@@ -21,22 +22,42 @@ long int cube(int num){
   return square(num) * num;
 };
 
+double simpleInterest(double principalAmount, double rate, double time){
+  return (principalAmount*rate*time)/100;
+}
+
 
 int main(void){
-  int num;
+  int num1, num2, num3, num4, num5;
+  double principalAmount, rate, time;
+
   printf("Enter a number to check whether it is even or not: ");
-  scanf("%d", &num);
-  printf("%d is %s", num, isEven(num)? "even":"not even");
+  scanf("%d", &num1);
+  printf("%d is %s", num1, isEven(num1)? "even":"not even");
 
   printf("\n\nEnter a number to check whether it is odd or not: ");
-  scanf("%d", &num);
-  printf("%d is %s", num, isOdd(num)? "odd":"not odd");
+  scanf("%d", &num2);
+  printf("%d is %s", num2, isOdd(num2)? "odd":"not odd");
 
   printf("\n\nEnter a number to get square of it: ");
-  scanf("%d", &num);
-  printf("Square of %d is %ld", num, square(num));
+  scanf("%d", &num3);
+  printf("Square of %d is %ld", num3, square(num3));
 
   printf("\n\nEnter a number to get cube of it: ");
-  scanf("%d", &num);
-  printf("Cube of %d is %ld", num, cube(num));
+  scanf("%d", &num4);
+  printf("Cube of %d is %ld", num4, cube(num4));
+
+  printf("\n\nEnter a number to get cube of it: ");
+  scanf("%d", &num5);
+  printf("Cube of %d is %ld", num5, cube(num5));
+
+  printf("\n\nCalculate Simple Interest\nEnter Principal Amount: ");
+  scanf("%lf", &principalAmount);
+  printf("Enter Rate: ");
+  scanf("%lf", &rate);
+  printf("Enter Time Period: ");
+  scanf("%lf", &time);
+  printf("Simple Interest: %lf", simpleInterest(principalAmount,rate,time));
+
 };
+
