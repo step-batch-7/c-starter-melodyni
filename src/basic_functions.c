e6e7a9f6c840405c220687bde235f9c069a64301
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-03-30 13:14:43 +0530

    added cube function

--- src/basic_functions.c
+++ src/basic_functions.c
@@ -3,6 +3,7 @@
 char isEven(int num);
 char isOdd(int num);
 long int square(int num);
+long int cube(int num);
 
 char isEven(int num){
   return num % 2 == 0;
@@ -14,6 +15,10 @@ char isOdd(int num){
 
 long int square(int num){
   return num * num;
+};
+
+long int cube(int num){
+  return square(num) * num;
 };
 
 
@@ -30,4 +35,8 @@ int main(void){
   printf("\n\nEnter a number to get square of it: ");
   scanf("%d", &num);
   printf("Square of %d is %ld", num, square(num));
+
+  printf("\n\nEnter a number to get cube of it: ");
+  scanf("%d", &num);
+  printf("Cube of %d is %ld", num, cube(num));
 };
